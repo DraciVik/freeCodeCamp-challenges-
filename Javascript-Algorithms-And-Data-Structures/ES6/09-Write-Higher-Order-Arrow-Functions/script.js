7adc91d30ce43e2bbaf9ed7e41c51410b9e6dec1
DraciVik <viktor.stojanov90@gmail.com>
2018-04-05 21:44:58 +0200

    Finished 10 exercises of the ES6 track

--- Javascript-Algorithms-And-Data-Structures/ES6/09-Write-Higher-Order-Arrow-Functions/script.js
+++ Javascript-Algorithms-And-Data-Structures/ES6/09-Write-Higher-Order-Arrow-Functions/script.js
@@ -16,3 +16,20 @@ This code is more succinct and accomplishes the same task with fewer lines of co
 
 Use arrow function syntax to compute the square of only the positive integers (fractions are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers.
 */
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.reduce((myArr, num) => {
+    if(num % 1 === 0) {
+      myArr.push(num * num);
+    }
+    return myArr;
+  },[]);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
