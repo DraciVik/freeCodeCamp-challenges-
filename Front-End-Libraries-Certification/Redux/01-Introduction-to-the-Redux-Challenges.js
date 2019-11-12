8c68111d7862cd5ee6313d0346348464b7a3a16f
DraciVik <viktor.stojanov90@gmail.com>
2019-11-12 23:39:28 +0100

    Solved a couple of additional Redux challenges

--- Front-End-Libraries-Certification/Redux/01-Introduction-to-the-Redux-Challenges.js
+++ Front-End-Libraries-Certification/Redux/01-Introduction-to-the-Redux-Challenges.js
@@ -1,1 +1,1 @@
-//They still have not added a description
+// Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test. While you can use Redux with any view library, it's introduced here before being combined with React.
