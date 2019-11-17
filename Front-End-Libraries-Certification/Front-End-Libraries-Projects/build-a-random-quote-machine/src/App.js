a9d063798afb359749de7003d8ebd5771b75541e
Viktor Stojanov <viktor.stojanov90@gmail.com>
2019-11-17 03:10:58 +0100

    Done for mobile first (#13)

--- Front-End-Libraries-Certification/Front-End-Libraries-Projects/build-a-random-quote-machine/src/App.js
+++ Front-End-Libraries-Certification/Front-End-Libraries-Projects/build-a-random-quote-machine/src/App.js
@@ -1,11 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./App.css";
+import { FaTwitterSquare } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <blockquote>
+      <main id="quote-box">
+        <blockquote id="text">
+          <span id="quotation-mark">&rdquo;</span>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
@@ -15,9 +18,16 @@ function App() {
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
+          <span id="quotation-mark">&rdquo;</span>
         </blockquote>
-        <p>Author</p>
-        <button type="button">New Quote</button>
+        <p id="author">
+          <span class="letter-spacing">&mdash;&mdash;&mdash;&mdash;</span>Yo
+          mamas ass
+        </p>
+        <button id="new-quote">New Quote</button>
+        <a class="button" id="tweet-quote" title="Tweet this!" target="_blank">
+          <FaTwitterSquare />
+        </a>
       </main>
     </div>
   );
