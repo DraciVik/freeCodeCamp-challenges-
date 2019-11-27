ff781864952621c3d8c9d1fe084a90cc135f8bf3
DraciVik <viktor.stojanov90@gmail.com>
2019-11-27 23:05:05 +0100

    Made drum pad work on keyboard

--- Front-End-Libraries-Certification/Front-End-Libraries-Projects/drum-machine/src/App.js
+++ Front-End-Libraries-Certification/Front-End-Libraries-Projects/drum-machine/src/App.js
@@ -1,27 +1,76 @@
 import React from "react";
+import drumData from "./components/drumData";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <main id="drum-machine">
-        <div id="drum-buttons">
-          <button className="drum-pad">Q</button>
-          <button className="drum-pad">W</button>
-          <button className="drum-pad">E</button>
-          <button className="drum-pad">A</button>
-          <button className="drum-pad">S</button>
-          <button className="drum-pad">D</button>
-          <button className="drum-pad">Z</button>
-          <button className="drum-pad">X</button>
-          <button className="drum-pad">C</button>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: ""
+    };
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    // const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    console.log(event);
+    if (!audio) return;
+    // key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+
+    this.setState({
+      display: "ok"
+    });
+  }
+
+  handleKeyPress(event) {
+    console.log(event); //TODOODODODODOODOD
+  }
+
+  render() {
+    const drums = drumData.map(drum => {
+      return (
+        <div
+          key={drum.key}
+          role="button"
+          tabIndex="0"
+          data-key={drum.dataKey}
+          className="drum-pad"
+          onClick={() => this.playSound}
+        >
+          {drum.key}
+          {/*eslint-disable-next-line jsx-a11y/media-has-caption*/}
+          <audio
+            data-key={drum.dataKey}
+            src={drum.src}
+            type="audio/mp3"
+          ></audio>
         </div>
-        <div id="display-container">
-          <p id="display">BullShit</p>
-        </div>
-      </main>
-    </div>
-  );
+      );
+    });
+
+    const { display } = this.state;
+
+    return (
+      <div className="App">
+        <main id="drum-machine">
+          <div id="drum-buttons">{drums}</div>
+          <div id="display-container">
+            <p id="display">{display}</p>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Front-End-Libraries-Certification/Front-End-Libraries-Projects/drum-machine/src/components/drumData.js
@@ -0,0 +1,65 @@
+const drumData = [
+  {
+    name: "Bdim Chord",
+    key: "Q",
+    dataKey: "81",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+
+  {
+    name: "C Chord",
+    key: "W",
+    dataKey: "87",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+
+  {
+    name: "A Chord",
+    key: "E",
+    dataKey: "69",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+
+  {
+    name: "B Chord",
+    key: "A",
+    dataKey: "65",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+
+  {
+    name: "Clap",
+    key: "S",
+    dataKey: "83",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+
+  {
+    name: "Open-HH",
+    key: "D",
+    dataKey: "68",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+
+  {
+    name: "Kick-'n-Hat",
+    key: "Z",
+    dataKey: "90",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+
+  {
+    name: "Kick",
+    key: "X",
+    dataKey: "88",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+
+  {
+    name: "Closed-HH",
+    key: "C",
+    dataKey: "67",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+export default drumData;
