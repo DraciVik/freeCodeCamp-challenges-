bac0603b4de4e27977de290bd0c84087eb677d40
DraciVik <viktor.stojanov90@gmail.com>
2020-01-15 04:30:15 +0100

    Styled the buttons and display output, added basic functionality

--- Front-End-Libraries-Certification/Front-End-Libraries-Projects/calculator/src/App.js
+++ Front-End-Libraries-Certification/Front-End-Libraries-Projects/calculator/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+      calculation: "0"
+    };
+  }
+
+  calculate = () => {
+    let newResult = eval(this.state.calculation);
+    this.setState({ result: newResult, calculation: "0" });
+  };
+
+  addNumberToCalculation = e => {
+    console.log(e.target.innerText);
+  };
+
+  render() {
+    return (
+      <div id="calculator">
+        <div id="display">
+          <div id="result">{this.state.result}</div>
+          <div id="calculation">{this.state.calculation}</div>
+        </div>
+        <div id="buttons">
+          <button id="clear">AC</button>
+          <button id="divide">/</button>
+          <button id="multiply">*</button>
+
+          <button id="seven" onClick={this.addNumberToCalculation}>
+            7
+          </button>
+          <button id="eight" onClick={this.addNumberToCalculation}>
+            8
+          </button>
+          <button id="nine" onClick={this.addNumberToCalculation}>
+            9
+          </button>
+          <button id="four" onClick={this.addNumberToCalculation}>
+            4
+          </button>
+          <button id="five" onClick={this.addNumberToCalculation}>
+            5
+          </button>
+          <button id="six" onClick={this.addNumberToCalculation}>
+            6
+          </button>
+          <button id="one" onClick={this.addNumberToCalculation}>
+            1
+          </button>
+          <button id="two" onClick={this.addNumberToCalculation}>
+            2
+          </button>
+          <button id="three" onClick={this.addNumberToCalculation}>
+            3
+          </button>
+          <button id="zero" onClick={this.addNumberToCalculation}>
+            0
+          </button>
+          <button id="decimal">.</button>
+
+          <button id="subtrac">-</button>
+          <button id="add">+</button>
+          <button id="equals" onClick={this.calculate}>
+            =
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
