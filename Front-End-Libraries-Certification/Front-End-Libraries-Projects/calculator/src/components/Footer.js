5f54ed4812fd6165a4a294a44c9e7f2720b94a98
DraciVik <viktor.stojanov90@gmail.com>
2020-01-23 22:53:05 +0100

    Added Footer component plus styling

---
+++ Front-End-Libraries-Certification/Front-End-Libraries-Projects/calculator/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <p className="footer-text">
+      Made with <span className="heart"></span> by{" "}
+      <a
+        className="profile-link"
+        href="https://github.com/dracivik"
+        target="_blank"
+      >
+        Viktor Stojanov{" "}
+      </a>
+    </p>
+  );
+};
+
+export default Footer;
